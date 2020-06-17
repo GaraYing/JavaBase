e435a1cc19f2ab07bceb5f40172ecf5792c74c0d
GaraYing <984851486@qq.com>
2020-06-17 15:29:58 +0800

    -- reduce on Java8

---
+++ src/com/gara/java8/ReduceDemo.java
@@ -0,0 +1,21 @@
+package com.gara.java8;
+
+import java.util.Objects;
+import java.util.stream.Stream;
+
+/**
+ * @description: reduce learn
+ * @author:  GaraYing
+ * @createTime: 2020/6/17 14:17
+ * @Version: 1.0
+**/
+public class ReduceDemo {
+
+    public static void main(String[] args) {
+        int count = Stream.of(1, 2, 3)
+                .reduce(0, (acc, element) -> acc + element);
+        Integer reduce = Stream.of(1, 2, 3)
+                .reduce(0, Integer::sum);
+        System.out.println(Objects.equals(count, reduce));
+    }
+}
