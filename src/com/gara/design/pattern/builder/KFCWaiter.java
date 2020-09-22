eb1f97cedb3f0a66cff6b199548a521932912abf
GaraYing <984851486@qq.com>
2020-09-22 11:03:31 +0800

    perf: design-pattern:builder:code polish

--- src/com/gara/design/pattern/builder/KFCWaiter.java
+++ src/com/gara/design/pattern/builder/KFCWaiter.java
@@ -1,5 +1,11 @@
 package com.gara.design.pattern.builder;
 
+/**
+ * @description: Director
+ * @author:  GaraYing
+ * @createTime: 2020/9/21 19:48
+ * @version: 1.0
+**/
 public class KFCWaiter {
 
     private MealBuilder builder;
--- src/com/gara/design/pattern/builder/MealBuilder.java
+++ src/com/gara/design/pattern/builder/MealBuilder.java
@@ -1,5 +1,11 @@
 package com.gara.design.pattern.builder;
 
+/**
+ * @description: abstract builder
+ * @author:  GaraYing
+ * @createTime: 2020/9/21 19:47
+ * @version: 1.0
+**/
 public abstract class MealBuilder {
 
     protected Meal meal = new Meal();
