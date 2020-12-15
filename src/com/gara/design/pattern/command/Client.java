8060c4d411d0ef72b344291edc604f4393d6ad48
GaraYing <984851486@qq.com>
2020-12-15 23:46:33 +0800

    perf: DesignPatterns#Command

--- src/com/gara/design/pattern/command/Client.java
+++ src/com/gara/design/pattern/command/Client.java
@@ -1,13 +1,22 @@
 package com.gara.design.pattern.command;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Client {
     public static void main(String[] args) {
         Receiver receiver = new Receiver();
         Command command = new ConcreteCommand(receiver);
+        Command command1 = new ConcreteCommand(receiver);
         Command command2 = new ConcreteCommand2(receiver);
-        Invoker invoker = new Invoker(command);
-        invoker.executeCommand();
-        invoker = new Invoker(command2);
+//        List<Command> commands = Arrays.asList(command, command1, command2);
+        List<Command> commands = new ArrayList<>(3);
+        commands.add(command2);
+        commands.add(command1);
+        commands.add(command);
+        Invoker invoker = new Invoker(commands);
+        invoker.removeCommand(command1);
         invoker.executeCommand();
     }
 }
--- src/com/gara/design/pattern/command/Invoker.java
+++ src/com/gara/design/pattern/command/Invoker.java
@@ -1,13 +1,42 @@
 package com.gara.design.pattern.command;
 
-public class Invoker {
-    private Command command;
+import sun.util.calendar.CalendarUtils;
 
-    public Invoker(Command command) {
-        this.command = command;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public class Invoker {
+    private List<Command> commands;
+
+    public Invoker(List<Command> commands) {
+        this.commands = commands;
     }
 
-    public void executeCommand(){
-        command.execute();
+    public void executeCommand() {
+        commands.forEach(Command::execute);
+    }
+
+    public void removeCommand(Command command) {
+        commands.removeIf(e->e.equals(command));
+//        Iterator<Command> iterator = commands.iterator();
+//        while (iterator.hasNext()) {
+//            Command c = iterator.next();
+//            if (c.equals(command)) {
+//                iterator.remove();
+//            }
+//        }
+        System.out.println(command + "is removed at " + Calendar.getInstance().getTime());
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 34);
+//        list.removeIf(e->e.equals(34));
+        List<Integer> collect = list.stream().filter(e -> !e.equals(34)).collect(Collectors.toList());
+        System.out.println(collect);
     }
 }
